78540ab0c27ebba0df18845e6aca70c3475293e4
Tom Harrop <twharrop@gmail.com>
2025-03-06 13:26:00 +1100

    check accession against ncbi

--- src/atol_bpa_datamapper/organism_mapper.py
+++ src/atol_bpa_datamapper/organism_mapper.py
@@ -2,6 +2,7 @@
 
 from .logger import logger
 from pathlib import Path
+import hashlib
 import pandas as pd
 import shelve
 import skbio.io
@@ -12,17 +13,35 @@ import skbio.io
 CACHE_DIR = Path("dev/taxdump_cache")
 
 
+def compute_sha256(file_path):
+    logger.debug(f"Computing sha256 checksum for {file_path}.")
+    sha256 = hashlib.sha256()
+    with open(file_path, "rb") as f:
+        for block in iter(lambda: f.read(4096), b""):
+            sha256.update(block)
+    hex_digest = sha256.hexdigest()
+    logger.debug(f"Checksum: {hex_digest}")
+    return hex_digest
+
+
 def read_taxdump_file(file_path, scheme):
     cache_file = Path(CACHE_DIR, f"{Path(file_path).stem}_{scheme}.db")
     Path.mkdir(cache_file.parent, exist_ok=True, parents=True)
+    current_checksum = compute_sha256(file_path)
+
     with shelve.open(cache_file) as cache:
-        if "data" in cache:
+        if (
+            "data" in cache
+            and "checksum" in cache
+            and cache["checksum"] == current_checksum
+        ):
             logger.info(f"Reading {scheme} from cache {cache_file}")
             return cache["data"]
         else:
             data = skbio.io.read(file_path, "taxdump", into=pd.DataFrame, scheme=scheme)
             logger.info(f"Writing {scheme} to cache {cache_file}")
             cache["data"] = data
+            cache["checksum"] = current_checksum
             return data
 
 
@@ -50,8 +69,9 @@ class OrganismSection(dict):
     def __init__(self, package_data, ncbi_taxdump):
         super().__init__()
         self.update(package_data)
-        print(self)
         self.has_taxid = self.get("taxon_id") not in [None, "", "0", "0.0"]
+
+        # get the taxid
         if self.has_taxid:
             self.raw_taxid = self.get("taxon_id")
         else:
@@ -60,6 +80,26 @@ class OrganismSection(dict):
 
         if self.raw_taxid != None:
             self.format_taxid()
+
+        self.check_ncbi_taxonomy_for_taxid(ncbi_taxdump)
+
+    def check_ncbi_taxonomy_for_taxid(self, ncbi_taxdump):
+        # Check if it's an NCBI taxid
+        if self.has_taxid:
+            self.taxid_is_ncbi_node = self.taxid in ncbi_taxdump.nodes.index
+        else:
+            self.taxid_is_ncbi_node = False
+
+        if self.has_taxid and self.taxid_is_ncbi_node:
+            self.rank = ncbi_taxdump.get_rank(self.taxid)
+            self.scientific_name = ncbi_taxdump.get_scientific_name_txt(self.taxid)
+            self.scientific_name_source = "ncbi"
+        else:
+            self.rank = None
+            self.scientific_name = None
+            self.scientific_name_source = None
+
+        self.has_species_level_taxid = self.rank == "species"
 
     def format_taxid(self):
         # Check if the taxid is an Int
@@ -77,5 +117,6 @@ class OrganismSection(dict):
             self.raw_taxid_coerced_to_int = False
             self.taxid = None
         except ValueError:
-            print(self.raw_taxid)
-            quit(1)
+            self.raw_taxid_coerced_to_int = False
+            self.taxid = None
+            self.taxid_is_ncbi_node = False
--- src/atol_bpa_datamapper/resolve_organism.py
+++ src/atol_bpa_datamapper/resolve_organism.py
@@ -1,5 +1,5 @@
 from .arg_parser import parse_args_for_mapping
-from .io import read_input, OutputWriter
+from .io import read_input, OutputWriter, write_mapping_log_to_csv
 from .logger import logger, setup_logger
 from .organism_mapper import OrganismSection, NcbiTaxdump
 from .config_parser import MetadataMap
@@ -7,23 +7,38 @@ from .config_parser import MetadataMap
 
 nodes_file = "dev/nodes.dmp"
 names_file = "dev/names.dmp"
+mapping_log_file = "test/organism_mapping_log.csv.gz"
 
 
 def main():
+
+    max_iterations = 100
 
     args = parse_args_for_mapping()
     setup_logger(args.log_level)
 
     ncbi_taxdump = NcbiTaxdump(nodes_file, names_file)
 
+    mapping_log = {}
+    n_packages = 0
+
     bpa_to_atol_map = MetadataMap(args.field_mapping_file, args.value_mapping_file)
     input_data = read_input(args.input)
     for package in input_data:
+        n_packages += 1
         package.map_metadata(bpa_to_atol_map)
+        # TODO: parallelise, this is currently very slow.
         organism_section = OrganismSection(
             package.mapped_metadata["organism"], ncbi_taxdump
         )
-        print(organism_section.__dict__)
-        if organism_section.has_taxid:
-            print(ncbi_taxdump.get_rank(organism_section.taxid))
-            print(ncbi_taxdump.get_scientific_name_txt(organism_section.taxid))
+
+        # just for now, do this properly later
+        mapping_log[package.id] = [organism_section.__dict__]
+
+        if n_packages % 10 == 0:
+            logger.info(f"Processed {n_packages} packages")
+
+        if max_iterations and n_packages >= max_iterations:
+            break
+
+    write_mapping_log_to_csv(mapping_log, mapping_log_file)
