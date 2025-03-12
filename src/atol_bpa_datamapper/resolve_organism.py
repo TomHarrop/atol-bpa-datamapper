4e41c14556504e75948a43c6e9672435131a7a86
Tom Harrop <twharrop@gmail.com>
2025-03-12 14:48:18 +1100

    remove profiling

--- src/atol_bpa_datamapper/resolve_organism.py
+++ src/atol_bpa_datamapper/resolve_organism.py
@@ -4,11 +4,6 @@ from .io import read_input, OutputWriter, write_mapping_log_to_csv
 from .logger import logger, setup_logger
 from .organism_mapper import OrganismSection, NcbiTaxdump
 
-# profiling
-import cProfile
-import pstats
-from io import StringIO
-
 nodes_file = "dev/nodes.dmp"
 names_file = "dev/names.dmp"
 mapping_log_file = "test/organism_mapping_log.csv.gz"
@@ -17,7 +12,7 @@ mapping_log_file = "test/organism_mapping_log.csv.gz"
 def main():
 
     # debugging options
-    max_iterations = 10
+    max_iterations = 100
     manual_record = None
 
     args = parse_args_for_mapping()
@@ -27,9 +22,6 @@ def main():
     ncbi_taxdump = NcbiTaxdump(nodes_file, names_file, resolve_to_rank="species")
     bpa_to_atol_map = MetadataMap(args.field_mapping_file, args.value_mapping_file)
     input_data = read_input(args.input)
-
-    pr = cProfile.Profile()
-    pr.enable()
 
     n_packages = 0
     mapping_log = {}
@@ -56,12 +48,5 @@ def main():
 
             if n_packages % 10 == 0:
                 logger.info(f"Processed {n_packages} packages")
-
-    pr.disable()
-    s = StringIO()
-    sortby = "cumulative"
-    ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
-    ps.print_stats()
-    logger.warning(s.getvalue())
 
     write_mapping_log_to_csv(mapping_log, mapping_log_file)
