106572e8e081edb59a6b612a09fe7e413de2054d
Tom Harrop <twharrop@gmail.com>
2025-04-28 09:51:46 +1000

    chg: update test script

--- dev/scripts/test_commands.sh
+++ dev/scripts/test_commands.sh
@@ -1,17 +1,24 @@
+set -eux
+
+INPUT_DATA="dev/datasets.jsonl.gz"
+RESULT_DIR="$(date -I)_${RANDOM}"
+
+[ -d "results/${RESULT_DIR}" ] && exit 1 || mkdir -p "results/${RESULT_DIR}"
+
 filter-packages \
-    --raw_field_usage results/2025-04-17/raw_field_usage_filtering.jsonl.gz \
-    --bpa_field_usage results/2025-04-17/bpa_field_usage.jsonl.gz \
-    --bpa_value_usage results/2025-04-17/bpa_value_usage.jsonl.gz \
-    --decision_log results/2025-04-17/decision_log.csv.gz \
-    <dev/datasets.jsonl.gz \
-    >results/2025-04-17/f.jsonl.gz
+    --raw_field_usage "results/${RESULT_DIR}/raw_field_usage_filtering.jsonl.gz" \
+    --bpa_field_usage results/${RESULT_DIR}/bpa_field_usage.jsonl.gz \
+    --bpa_value_usage "results/${RESULT_DIR}/bpa_value_usage.jsonl.gz" \
+    --decision_log "results/${RESULT_DIR}/decision_log.csv.gz" \
+    <"${INPUT_DATA}" \
+    >"results/${RESULT_DIR}/f.jsonl.gz"
 
 map-metadata \
-    --raw_field_usage results/2025-04-17/raw_field_usage_mapping.jsonl.gz \
-    --raw_value_usage results/2025-04-17/raw_value_usage.jsonl.gz \
-    --mapped_field_usage results/2025-04-17/mapped_field_usage.jsonl.gz \
-    --mapped_value_usage results/2025-04-17/mapped_value_usage.jsonl.gz \
-    --unused_field_counts results/2025-04-17/unused_field_counts.jsonl.gz \
-    --mapping_log results/2025-04-17/decision_log.csv.gz \
-    <results/2025-04-17/f.jsonl.gz \
-    >results/2025-04-17/m.jsonl.gz
+    --raw_field_usage "results/${RESULT_DIR}/raw_field_usage_mapping.jsonl.gz" \
+    --raw_value_usage "results/${RESULT_DIR}/raw_value_usage.jsonl.gz" \
+    --mapped_field_usage "results/${RESULT_DIR}/mapped_field_usage.jsonl.gz" \
+    --mapped_value_usage "results/${RESULT_DIR}/mapped_value_usage.jsonl.gz" \
+    --unused_field_counts "results/${RESULT_DIR}/unused_field_counts.jsonl.gz" \
+    --mapping_log "results/${RESULT_DIR}/decision_log.csv.gz" \
+    <"results/${RESULT_DIR}/f.jsonl.gz" \
+    >"results/${RESULT_DIR}/m.jsonl.gz"
