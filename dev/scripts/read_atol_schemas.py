1fd2c92ed9a21aa5b321e978fb53e148620e90ea
Tom Harrop <twharrop@gmail.com>
2025-05-02 13:20:09 +1000

    chg: parse controlled vocab

--- dev/scripts/read_atol_schemas.py
+++ dev/scripts/read_atol_schemas.py
@@ -22,6 +22,12 @@ reads_scema = (
     "/export?gid=1743767073&format=tsv"
 )
 
+vocabulary_file = (
+    "https://docs.google.com/spreadsheets/d/"
+    "1ml5hASZ-qlAuuTrwHeGzNVqqe1mXsmmoDTekd6d9pto"
+    "/export?gid=1596130605&format=tsv"
+)
+
 
 def read_schema(schema_file):
     df = pd.read_csv(
@@ -44,6 +50,24 @@ def read_schema(schema_file):
     return df
 
 
+def read_vocabulary(vocabulary_file):
+    df = pd.read_csv(
+        vocabulary_file,
+        delimiter="\t",
+        header=0,
+        usecols=[0, 1, 2, 3],
+        names=["category", "atol_field", "atol_value", "allowed_value"],
+        dtype={
+            "category": pd.StringDtype,
+            "atol_field": pd.StringDtype,
+            "atol_value": pd.StringDtype,
+            "allowed_value": pd.StringDtype,
+        },
+        na_values=["", "[unmapped]"],
+    )
+    return df
+
+
 def sanitise_field_name(field_string):
     allowed_chars = re.compile("[a-zA-Z0-9 _]+")
     match = allowed_chars.match(field_string)
@@ -57,6 +81,7 @@ def sanitise_field_name(field_string):
 
 def main():
 
+    # the field mappings
     sample_data = read_schema(sample_schema)
     reads_data = read_schema(reads_scema)
 
@@ -85,6 +110,34 @@ def main():
     Path(json_output_file.parent).mkdir(parents=True, exist_ok=True)
     with open(json_output_file, mode="w", encoding="utf-8") as json_file:
         json.dump(output_data, json_file, indent=4)
+
+    # the controlled vocabs
+    vocabulary = read_vocabulary(vocabulary_file)
+    vocab_dict = defaultdict(lambda: defaultdict(lambda: defaultdict(set)))
+    for _, row in vocabulary.iterrows():
+        atol_field = row["atol_field"].strip()
+        category = row["category"].strip()
+        atol_value = row["atol_value"].strip()
+        allowed_value = row["allowed_value"].strip()
+        # print(category, atol_field, atol_value, allowed_value)
+        vocab_dict[category][atol_field][atol_value].update([allowed_value])
+
+    vocab_output = {
+        category: {
+            atol_field: {
+                atol_value: list(sorted(allowed_values))
+                for atol_value, allowed_values in atol_values.items()
+            }
+            for atol_field, atol_values in fields.items()
+        }
+        for category, fields in vocab_dict.items()
+    }
+
+    # Write the JSON output
+    vocab_output_file = Path("results", outdir, "value_mapping_bpa_to_atol.json")
+    Path(vocab_output_file.parent).mkdir(parents=True, exist_ok=True)
+    with open(vocab_output_file, mode="w", encoding="utf-8") as json_file:
+        json.dump(vocab_output, json_file, indent=4)
 
 
 if __name__ == "__main__":
