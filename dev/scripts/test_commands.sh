b250ef49a8d5d6a666d5014f211dce634363cb0c
Tom Harrop <twharrop@gmail.com>
2025-05-26 10:03:06 +1000

    run manual tests

--- dev/scripts/test_commands.sh
+++ dev/scripts/test_commands.sh
@@ -5,6 +5,14 @@ RESULT_DIR="$(date -I)_${RANDOM}"
 
 [ -d "results/${RESULT_DIR}" ] && exit 1 || mkdir -p "results/${RESULT_DIR}"
 
+if [ ! -f "${INPUT_DATA}" ]; then
+    ckanapi search datasets \
+        -a "${BPI_APIKEY}" \
+        include_private=true \
+        -O "${INPUT_DATA}" \
+        -z -r https://data.bioplatforms.com
+fi
+
 filter-packages \
     --raw_field_usage "results/${RESULT_DIR}/raw_field_usage_filtering.jsonl.gz" \
     --bpa_field_usage results/${RESULT_DIR}/bpa_field_usage.jsonl.gz \
@@ -14,13 +22,12 @@ filter-packages \
     >"results/${RESULT_DIR}/f.jsonl.gz"
 
 map-metadata \
-    -l DEBUG \
     --raw_field_usage "results/${RESULT_DIR}/raw_field_usage_mapping.jsonl.gz" \
     --raw_value_usage "results/${RESULT_DIR}/raw_value_usage.jsonl.gz" \
     --mapped_field_usage "results/${RESULT_DIR}/mapped_field_usage.jsonl.gz" \
     --mapped_value_usage "results/${RESULT_DIR}/mapped_value_usage.jsonl.gz" \
     --unused_field_counts "results/${RESULT_DIR}/unused_field_counts.jsonl.gz" \
-    --mapping_log "results/${RESULT_DIR}/decision_log.csv.gz" \
+    --mapping_log "results/${RESULT_DIR}/mapping_log.csv.gz" \
     --sanitization_changes "results/${RESULT_DIR}/sanitization_changes.jsonl.gz" \
     <"results/${RESULT_DIR}/f.jsonl.gz" \
     >"results/${RESULT_DIR}/m.jsonl.gz"
