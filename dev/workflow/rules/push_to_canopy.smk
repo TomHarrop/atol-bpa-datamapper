02e907779d09d5ae3350c6b4eea550a3a9a65aa2
Tom Harrop <twharrop@gmail.com>
2026-08-03 17:10:11 +1000

    quick result push to canopy !wip

--- dev/workflow/rules/push_to_canopy.smk
+++ dev/workflow/rules/push_to_canopy.smk
@@ -1,6 +1,27 @@
 #!/usr/bin/env python3
 
+
+import requests
+import urllib
+
 from os import getenv
+
+
+def canopy_login(canopy_username: str, canopy_password: str) -> str:
+    # log in to API
+    login = requests.post(
+        urllib.parse.urljoin(_api_url, "auth/login"),
+        data={"username": canopy_username, "password": canopy_password},
+    )
+
+    # Stop if login failed
+    login.raise_for_status()
+
+    canopy_token = login.json().get("access_token")
+
+    auth_header = {"Authorization": f"Bearer {canopy_token}"}
+
+    return auth_header
 
 
 def check_env_var(env_var_name: str) -> str:
@@ -10,6 +31,10 @@ def check_env_var(env_var_name: str) -> str:
     return env_var_value
 
 
+# FIXME. Hard coded defaults for now.
+_api_url = "https://api.atol.test.biocommons.org.au/api/v1/"
+
+
 envvars:
     "CANOPY_USERNAME",
     "CANOPY_PASSWORD",
@@ -17,16 +42,15 @@ envvars:
 
 rule organisms_bulk_import:
     input:
-        unique_organisms=Path(
-            result_path, "transform_data", "unique_organisms.jsonl.gz"
-        ),
+        request_body=Path(result_path, "transform_data", "unique_organisms.json.gz"),
+    output:
+        response=Path(result_path, "canopy_import", "organisms_bulk_import.json"),
     container:
         container_uri if use_container else None
     params:
-        request_type="POST",
-        endpoint="/api/v1/organisms/bulk-import",
-        body="",
-        canopy_username=check_env_var("CANOPY_USERNAME"),
-        canopy_password=check_env_var("CANOPY_PASSWORD"),
+        endpoint="organisms_bulk_import",
+        auth_header=canopy_login(
+            check_env_var("CANOPY_USERNAME"), check_env_var("CANOPY_PASSWORD")
+        ),
     script:
         "../scripts/canopy_client.py"
--- dev/workflow/scripts/canopy_client.py
+++ dev/workflow/scripts/canopy_client.py
@@ -1,41 +1,54 @@
 #!/usr/bin/env python3
 
-
-import requests
+from collections.abc import Generator
+import gzip
+import json
+from pathlib import Path
 import urllib
 
+from atol_bpa_datamapper.io import read_gzip_textfile
+import requests
 
-def canopy_login(canopy_username: str, canopy_password: str) -> str:
-    # log in to API
-    login = requests.post(
-        urllib.parse.urljoin(_api_url, _endpoints.get("auth_login")),
-        data={"username": canopy_username, "password": canopy_password},
-    )
 
-    # Stop if login failed
-    login.raise_for_status()
+def get_request_body(json_file: str) -> Generator[dict[str, str]]:
+    if json_file.endswith(".gz"):
+        f = gzip.open(json_file, "rt")
+    else:
+        raise NotImplementedError(json_file)
 
-    canopy_token = login.json().get("access_token")
-
-    auth_header = {"Authorization": f"Bearer {canopy_token}"}
-
-    return auth_header
+    return json.loads(f.read())
 
 
 # FIXME. Hard coded defaults for now.
 _api_url = "https://api.atol.test.biocommons.org.au/api/v1/"
 
-_endpoints = {
-    "auth_login": "auth/login",
-}
+_endpoints = {"organisms_bulk_import": ("/api/v1/organisms/bulk-import", "POST")}
 
 
 def main():
 
-    deets = canopy_login(
-        snakemake.params["canopy_username"], snakemake.params["canopy_password"]
+    request_header = snakemake.params["auth_header"]
+
+    request_body = None
+    if snakemake.input["request_body"]:
+        request_body = get_request_body(snakemake.input["request_body"])
+
+    endpoint_url, request_type = _endpoints.get(
+        snakemake.params["endpoint"], (None, None)
     )
-    raise ValueError(deets)
+
+    request_url = urllib.parse.urljoin(_api_url, endpoint_url)
+
+    if request_type == "GET":
+        raise NotImplementedError("TODO implement GET")
+    if request_type == "POST":
+        response = requests.post(
+            request_url, headers=request_header, data=json.dumps(request_body)
+        )
+
+    if snakemake.output["response"]:
+        with open(snakemake.output["response"], "wt") as f:
+            f.write(json.dumps(response.json()))
 
 
 if __name__ == "__main__":
